11424bd126566077dd1ccc18330be10369b1ebef
Jmimiding4104@gmail.com <Jmimiding4104>
2024-05-06 04:00:32 +0800

    init

--- service/handleErrorAsync.js
+++ service/handleErrorAsync.js
@@ -4,11 +4,7 @@ function handleErrorAsync(fn) {
   return function (req, res, next) {
     fn(req, res, next)
       .catch((err) => {
-        if (err.name === 'CastError' && err.kind === 'ObjectId') {
-          return next(err);
-        } else {
-          return next(err);
-        }
+        return next(err);
       });
   };
 }
